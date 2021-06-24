5810b932cb85edd2e235112b73d9c36e77fb315b
Martynov Aleksandr <a.martynov@trinity.local>
2021-06-24 16:45:48 +0300

    commit

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+import '../styles/index.scss';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
+if (process.env.NODE_ENV === 'development') {
+    require('../index.html');
+}
